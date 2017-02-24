fa31200c1c3fec25187f01aeb38f954412a7e3d2
Mel Liu <privatemiao@gmail.com>
2017-02-24 18:02:32 +0800

    a

---
+++ src/main/java/org/mel/pds/Application.java
@@ -0,0 +1,7 @@
+package org.mel.pds;
+
+public class Application {
+	public static void main(String[] args) {
+		
+	}
+}
--- src/main/java/pds/Application.java
+++
@@ -1,7 +0,0 @@
-package pds;
-
-public class Application {
-	public static void main(String[] args) {
-		
-	}
-}
