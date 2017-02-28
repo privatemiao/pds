4fd9e8db375d84d40f3110160a0cad44d708a94f
Mel Liu <privatemiao@gmail.com>
2017-02-28 13:38:02 +0800

    a

--- src/main/java/org/mel/pds/MainFrame.java
+++ src/main/java/org/mel/pds/MainFrame.java
@@ -4,15 +4,12 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.HeadlessException;
 
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
 
 public class MainFrame extends JFrame {
 
@@ -38,18 +35,6 @@ public class MainFrame extends JFrame {
 	private Container createContentPane() {
 		JPanel panel = new JPanel(new BorderLayout());
 		panel.setOpaque(true);
-
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBackground(Color.WHITE);
-		panel.add(scrollPane, BorderLayout.CENTER);
-		
-		for (int i = 0; i < 200; i ++){
-			scrollPane.add(new JLabel("Welcome " + (i + 1) ));
-		}
-		
-		
-		
-		
 		panel.setBackground(Color.WHITE);
 		return panel;
 	}
@@ -57,10 +42,10 @@ public class MainFrame extends JFrame {
 	private JMenuBar createMenuBar() {
 		JMenuBar menuBar = new JMenuBar();
 
-		//first menu.
+		// first menu.
 		JMenu menu = new JMenu("A Menu");
 		menuBar.add(menu);
-		
+
 		return menuBar;
 	}
 
