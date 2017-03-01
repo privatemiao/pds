3e8c9a19f7a70af0d56f98c0c87ad9a91f656056
Mel Liu <privatemiao@gmail.com>
2017-03-01 10:36:58 +0800

    a

--- src/main/java/org/mel/pds/Service.java
+++ src/main/java/org/mel/pds/Service.java
@@ -45,24 +45,24 @@ public class Service {
 			@Override
 			public void actionPerformed(ActionEvent event) {
 				Node node = nodeIndex.get(event.getActionCommand());
-				if (StringUtils.isEmpty(event.getActionCommand()) || node == null){
+				if (StringUtils.isEmpty(event.getActionCommand()) || node == null) {
 					mainFrame.showMessage("This operation not pair yet!");
 					return;
 				}
-				
+
 				Module module = node.getModule();
-				if (module == null){
+				if (module == null) {
 					mainFrame.showMessage("This operation not ready yet!");
 					return;
 				}
-				
+
 				try {
 					Class<?> modelClass = Class.forName(module.getModel());
 					Class<?> viewClass = Class.forName(module.getView());
 					Container container = (Container) viewClass.newInstance();
-					if (modelClass == null){
+					if (modelClass == null) {
 						Class.forName(module.getController()).getConstructor(viewClass).newInstance(container);
-					}else{
+					} else {
 						Class.forName(module.getController()).getConstructor(viewClass, modelClass).newInstance(container, modelClass.newInstance());
 					}
 					mainFrame.setContentPane(container);
@@ -76,24 +76,23 @@ public class Service {
 	}
 
 	private void indexNodes() {
-		if (CollectionUtils.isEmpty(nodes)){
+		if (CollectionUtils.isEmpty(nodes)) {
 			return;
 		}
-		
+
 		for (Node node : nodes) {
 			nodeIndex.put(node.getId(), node);
-			if (!CollectionUtils.isEmpty(node.getNodes())){
+			if (!CollectionUtils.isEmpty(node.getNodes())) {
 				indexSubNodes(node.getNodes());
 			}
 		}
-			
-	}
 
+	}
 
 	private void indexSubNodes(List<Node> nodes) {
 		for (Node node : nodes) {
 			nodeIndex.put(node.getId(), node);
-			if (!CollectionUtils.isEmpty(node.getNodes())){
+			if (!CollectionUtils.isEmpty(node.getNodes())) {
 				indexSubNodes(node.getNodes());
 			}
 		}
