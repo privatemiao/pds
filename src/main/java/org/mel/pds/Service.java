27f4d9923c4c060da6fdfdebe18c3d5f76fa7cf5
Mel Liu <privatemiao@gmail.com>
2017-03-01 10:07:46 +0800

    a

--- src/main/java/org/mel/pds/Service.java
+++ src/main/java/org/mel/pds/Service.java
@@ -9,12 +9,16 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.lang3.StringUtils;
+import org.mel.pds.Node.Module;
+import org.springframework.util.CollectionUtils;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Service {
 
 	private List<Node> nodes = null;
-	private Map<String, MenuActionListener> menuListeners = new HashMap<>();
+	private Map<String, Node> nodeIndex = new HashMap<>();
 	private MainFrame mainFrame = null;
 
 	Service(MainFrame mainFrame) {
@@ -26,43 +30,73 @@ public class Service {
 			if (node != null && node.getNodes().size() > 0) {
 				nodes = node.getNodes();
 			}
+			if (CollectionUtils.isEmpty(nodes)) {
+				nodes = Collections.emptyList();
+			} else {
+				indexNodes();
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
-		prepareMenuListener();
 		this.mainFrame.setNodes(nodes);
 		this.mainFrame.setMenuListener(new ActionListener() {
 
 			@Override
-			public void actionPerformed(ActionEvent e) {
-				MenuActionListener listener = menuListeners.get(e.getActionCommand());
-				if (listener == null) {
-					mainFrame.showMessage("There is not handler for this menu.");
+			public void actionPerformed(ActionEvent event) {
+				Node node = nodeIndex.get(event.getActionCommand());
+				if (StringUtils.isEmpty(event.getActionCommand()) || node == null){
+					mainFrame.showMessage("This operation not pair yet!");
 					return;
 				}
-				listener.doProcess(e);
+				
+				Module module = node.getModule();
+				if (module == null){
+					mainFrame.showMessage("This operation not ready yet!");
+					return;
+				}
+				
+				try {
+					Class<?> modelClass = Class.forName(module.getModel());
+					Class<?> viewClass = Class.forName(module.getView());
+					Container container = (Container) viewClass.newInstance();
+					if (modelClass == null){
+						Class.forName(module.getController()).getConstructor(viewClass).newInstance(container);
+					}else{
+						Class.forName(module.getController()).getConstructor(viewClass, modelClass).newInstance(container, modelClass.newInstance());
+					}
+					mainFrame.setContentPane(container);
+					mainFrame.repaint();
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
 			}
 		});
 		this.mainFrame.setUpUI();
 	}
 
-	private void prepareMenuListener() {
-		menuListeners.put("file.photo.upload", new MenuActionListener() {
-
-			@Override
-			public void doProcess(ActionEvent event) {
-				try {
-					Container c = (Container) Class.forName("org.mel.pds.photo.upload.UploadView").newInstance();
-					mainFrame.setContentPane(c);
-					mainFrame.repaint();
-				} catch (Exception e) {
-					e.printStackTrace();
-					mainFrame.showMessage(e.getMessage());
-				}
+	private void indexNodes() {
+		if (CollectionUtils.isEmpty(nodes)){
+			return;
+		}
+		
+		for (Node node : nodes) {
+			nodeIndex.put(node.getId(), node);
+			if (!CollectionUtils.isEmpty(node.getNodes())){
+				indexSubNodes(node.getNodes());
 			}
-		});
+		}
+			
+	}
 
+
+	private void indexSubNodes(List<Node> nodes) {
+		for (Node node : nodes) {
+			nodeIndex.put(node.getId(), node);
+			if (!CollectionUtils.isEmpty(node.getNodes())){
+				indexSubNodes(node.getNodes());
+			}
+		}
 	}
 
 	public List<Node> getNodes() {
